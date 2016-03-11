bfd66e15dec62b99127ec2d3f42f7fd21f801f9e
Jack <gqing@uncc.edu>
2016-03-11 11:35:51 -0500

    Second Change

--- src/main/java/com/LearnSpring/HelloWorld/HelloWorldApplication.java
+++ src/main/java/com/LearnSpring/HelloWorld/HelloWorldApplication.java
@@ -1,7 +1,7 @@
 package com.LearnSpring.HelloWorld;
 
 // First change 
-
+// Another Change
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
